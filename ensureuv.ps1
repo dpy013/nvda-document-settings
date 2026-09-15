1133ba26f91e7d32f0883a070ab87931c2a9fe62
xingkong <26911141+dpy013@users.noreply.github.com>
2026-09-15 19:41:57 +0800

    Use latest stable uv (#1)
    * Use latest stable uv
    
    * Update GitHub Actions versions

--- ensureuv.ps1
+++ ensureuv.ps1
@@ -6,23 +6,10 @@ param(
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
-[Version]$UvVersion = '0.12.5'
 
 $hasUv = [bool](Get-Command uv -ErrorAction SilentlyContinue)
 if (-not $hasUv) {
-	Write-Error 'uv is not installed. Install uv first: https://docs.astral.sh/uv/getting-started/installation/'
-}
-
-try {
-	$json = uv self version --output-format json | ConvertFrom-Json
-	[Version]$installedVersion = $json.Version
-}
-catch {
-	[Version]$installedVersion = '0.0.0'
-}
-
-if ($installedVersion -lt $UvVersion) {
-	Write-Error "uv $UvVersion or newer is required. Installed version: $installedVersion"
+	Write-Error 'uv is not installed. Install the latest stable uv first: https://docs.astral.sh/uv/getting-started/installation/'
 }
 
 & uv @UvArgs
